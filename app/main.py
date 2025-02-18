2698ac79f482ca0cae1dbda19cd524cadb87d8f8
Denys Bokov <denys.bokov@jonasundderwolf.de>
2025-02-18 17:14:54 +0100

    fixup! feat: linter formatter solutions

--- app/main.py
+++ app/main.py
@@ -7,6 +7,7 @@ def format_linter_error(error: dict) -> dict:
         "source": "flake8",
     }
 
+
 def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
         "errors": [format_linter_error(error) for error in errors],
@@ -14,7 +15,9 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
         "status": "failed" if errors else "passed"
     }
 
+
 def format_linter_report(linter_report: dict) -> list:
-    return [format_single_linter_file(path, errors)
+    return [
+        format_single_linter_file(path, errors)
         for path, errors in linter_report.items()
     ]
